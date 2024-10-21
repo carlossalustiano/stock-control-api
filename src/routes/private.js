7e4ef2d00367c51679edf82635cfb95df83960ec
Carlos <carloseduardo17488@gmail.com>
2024-10-20 22:50:39 -0300

    Rotas privadas criadas com sucesso

--- src/routes/private.js
+++ src/routes/private.js
@@ -1,8 +1,78 @@
 import express, { request, response } from "express";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 const router = express.Router(); // Resgatando rotas do Express.
 const prisma = new PrismaClient(); // Inicializando o Prisma.
 
 export default router;
 
+// Criar um novo produto.
+
+router.post("/criar", async (request, response) => {
+  try {
+    await prisma.produtos.create({
+      data: {
+        name: request.body.name,
+        quantity: request.body.quantity,
+      }
+    });
+    response.status(201).json({ message: 'Produto criado com sucesso.' });
+  } catch (error) {
+    response.status(500).json({ message: 'Error no servidor, tente novamente.' });
+  }
+});
+
+// Listar todos os produtos ou um específico.
+
+router.get('/produtos', async (request, response) => {
+  try {
+    let produtos = [];
+    
+    if (request.query) {
+      produtos = await prisma.produtos.findMany({
+        where: {
+          name: request.query.name
+        }
+      })
+    } else {
+      produtos = await prisma.produtos.findMany();
+    }
+    response.status(200).json(produtos);
+  } catch (error) {
+    response.status(500).json({ message: 'Error no servidor, tente novamente.' });
+  }
+});
+
+// Atualizar informações de um produto.
+
+router.put('/produtos/:name', async (request, response) => {
+  try {
+    await prisma.produtos.update({
+      where: {
+        name: request.params.name
+      },
+      data: {
+        name: request.body.name,
+        quantity: request.body.quantity
+      }
+    })
+    response.status(201).json({ message: 'Informações do produto atualizadas.' });
+  } catch (error) {
+    response.status(500).json({ message: 'Error no servidor, tente novamente.' });
+  }
+});
+
+// Deletar um produto do estoque.
+
+router.delete('/produtos/:name', async (request, response) => {
+  try {
+    await prisma.produtos.delete({
+      where: {
+        name: request.params.name
+      }
+    })
+    response.status(200).json({ message: 'Produto removido do estoque.' })
+  } catch (error) {
+    response.status(500).json({ message: 'Error no servidor, tente novamente.' });
+  }
+});
